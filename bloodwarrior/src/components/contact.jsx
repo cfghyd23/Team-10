09c6ed1ca028090176bc2e337cef81dbd5dad2a8
Pravallika Reddy <pravallika7277>
2023-06-11 05:05:09 +0530

    contact

--- bloodwarrior/src/components/contact.jsx
+++ bloodwarrior/src/components/contact.jsx
@@ -151,7 +151,7 @@ export const Contact = (props) => {
           <p>
             &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
             <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
+              TemplateWir
             </a>
           </p>
         </div>
